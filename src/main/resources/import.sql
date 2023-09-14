17d12af72119d9ef37b8fe42ecb31ce4a3d78c36
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-14 15:54:29 +0200

    REPOSITORY COMPLETED

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO pizza (name, description, price) VALUES ("margherita", "pomodoro e mozzarella", 8.00);
 INSERT INTO pizza (name, description, price) VALUES ("marinara", "pomodoro e aglio", 6.00);
 INSERT INTO pizza (name, description, price) VALUES ("pugliese", "pomodoro, mozzarella e cipolla", 8.00);
+INSERT INTO pizza (name, description, price) VALUES ("napoletana", "pomodoro, olive, acciughe", 9.00);
+INSERT INTO pizza (name, description, price) VALUES ("pistacchiosa", "mozzarella, mortadella, pistacchio", 12.00);
+INSERT INTO pizza (name, description, price) VALUES ("bufala", "pomodoro, mozzarella di bufala", 10.00);
