d30184bc543a48966fd0436da8f71025f1109878
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-18 15:13:43 +0200

    added html for pizza's details with path from html pizzaList

--- src/main/java/org/lessons/java/spring/pizzeria/controller/PizzeriaController.java
+++ src/main/java/org/lessons/java/spring/pizzeria/controller/PizzeriaController.java
@@ -3,12 +3,16 @@ package org.lessons.java.spring.pizzeria.controller;
 import org.lessons.java.spring.pizzeria.model.Pizza;
 import org.lessons.java.spring.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -23,5 +27,17 @@ public class PizzeriaController {
         List<Pizza> pizzaList = pizzaRepository.findAll();
         model.addAttribute("pizzas", pizzaList);
         return "pizzaList";
+    }
+
+    @GetMapping ("/show/{pizzaId}")
+    public String show(@PathVariable("pizzaId") Integer id, Model model){
+        Optional<Pizza> pizzaOptional = pizzaRepository.findById(id);
+        if (pizzaOptional.isPresent()){
+            Pizza pizzaFromDB = pizzaOptional.get();
+            model.addAttribute("pizza", pizzaFromDB);
+            return "detail";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
