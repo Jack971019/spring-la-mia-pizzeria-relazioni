f007d20f28e0784aaa9f9d78655f746bec55b721
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-19 15:13:08 +0200

    added link from home page to create pizza- added html form to create a new pizza

--- src/main/java/org/lessons/java/spring/pizzeria/controller/PizzeriaController.java
+++ src/main/java/org/lessons/java/spring/pizzeria/controller/PizzeriaController.java
@@ -6,9 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -40,5 +39,18 @@ public class PizzeriaController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "/create";
+    }
+
+    @PostMapping("/create")
+    public String doCreate(@ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult) {
+        formPizza.setName(formPizza.getName().toUpperCase());
+        pizzaRepository.save(formPizza);
+        return "redirect:/";
     }
 }
